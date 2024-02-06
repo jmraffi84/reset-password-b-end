dbf759b92eaec7d5f98ace78b15d8dbdf92be16d
jmraffi84 <jmraffi@gmail.com>
2024-02-06 15:12:19 +0530

    chekking again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import helmet from 'helmet'
+// import helmet from 'helmet'
 dotenv.config()
 import { mongoConnection } from './db.js'
 import { userRouter } from './Routes/userAuth.js'
@@ -9,21 +9,15 @@ import { userRouter } from './Routes/userAuth.js'
 const PORT = process.env.PORT || 3500;
 const app = express();
 app.use(cors());
-
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     credentials: true,            //access-control-allow-credentials:true
-//     optionSuccessStatus: 200
-// }
-// app.use(cors());
+app.use(express.json())
 
 
-app.use(helmet());
+
+// app.use(helmet());
 
 mongoConnection()
 
 //in-built middleware for JSON
-app.use(express.json())
 app.use(userRouter)
 
 
